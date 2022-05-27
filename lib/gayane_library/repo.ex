aa81375f0a4f4c062cfba0c836caba2a27bcd917
Gayane <g_arshakyan@list.ru>
2022-05-27 18:02:57 +0300

    added find, fetch_one and find_by funcs to repo.ex

--- lib/gayane_library/repo.ex
+++ lib/gayane_library/repo.ex
@@ -2,4 +2,34 @@ defmodule GayaneLibrary.Repo do
   use Ecto.Repo,
     otp_app: :gayane_library,
     adapter: Ecto.Adapters.Postgres
+
+  def fetch_one(query) do
+    case __MODULE__.one(query) do
+      nil ->
+        {:error, :not_found}
+
+      value ->
+        {:ok, value}
+    end
+  end
+
+  def find(query, id) do
+    case __MODULE__.get(query, id) do
+      %_{} = schema ->
+        {:ok, schema}
+
+      nil ->
+        {:error, :not_found}
+    end
+  end
+
+  def find_by(query, by) do
+    case __MODULE__.get_by(query, by) do
+      %_{} = schema ->
+        {:ok, schema}
+
+      nil ->
+        {:error, :not_found}
+    end
+  end
 end
