d57b7edc9fa9d49a2990c76da0c9a7916e8a4896
Gayane <g_arshakyan@list.ru>
2022-06-09 22:43:46 +0300

    create general book context

---
+++ lib/gayane_library/books/books.ex
@@ -0,0 +1,25 @@
+defmodule GayaneLibrary.Books do
+  @moduledoc """
+  Describes the context of the book
+  """
+
+  alias GayaneLibrary.Books.Commands.{
+    CreateBook,
+    DeleteBook,
+    UpdateBook
+  }
+
+  alias GayaneLibrary.Books.Queries.{
+    GetBook,
+    ListBooks
+  }
+
+  # Commands
+  defdelegate create_book(attrs), to: CreateBook, as: :process
+  defdelegate delete_book(book), to: DeleteBook, as: :process
+  defdelegate update_book(book, attrs), to: UpdateBook, as: :process
+
+  # Queries
+  defdelegate get_book(id), to: GetBook, as: :process
+  defdelegate list_books(), to: ListBooks, as: :process
+end
