90f84f9e83b73544d49efb5d3ab35a882a8ac90f
Gayane <g_arshakyan@list.ru>
2022-06-09 21:30:36 +0300

    create books migration

---
+++ priv/repo/migrations/20220609170730_create_books.exs
@@ -0,0 +1,17 @@
+defmodule GayaneLibrary.Repo.Migrations.CreateBooks do
+  use Ecto.Migration
+
+  def change do
+    create table(:books) do
+      add :author, :string, default: ""
+      add :name, :string, null: false
+      add :text_content, :text
+      add :year, :integer
+      add :edition, :string, default: ""
+      add :hidden, :boolean, default: false, null: false
+      add :user_id, references(:users)
+
+      timestamps()
+    end
+  end
+end
