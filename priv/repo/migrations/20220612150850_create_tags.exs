644f5d6c1455065a14ed34a7fca4e11b5c338630
Gayane <g_arshakyan@list.ru>
2022-06-12 18:46:04 +0300

    create tags migration

---
+++ priv/repo/migrations/20220612150850_create_tags.exs
@@ -0,0 +1,14 @@
+defmodule GayaneLibrary.Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags) do
+      add :name, :string, null: false
+      add :description, :string, default: ""
+
+      timestamps()
+    end
+
+    create unique_index(:tags, [:name])
+  end
+end
