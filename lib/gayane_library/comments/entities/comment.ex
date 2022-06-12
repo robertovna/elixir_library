c84491d76cfd2567a005411979fb48b812381c0b
Gayane <g_arshakyan@list.ru>
2022-06-12 20:51:57 +0300

    create comments schema

---
+++ lib/gayane_library/comments/entities/comment.ex
@@ -0,0 +1,35 @@
+defmodule GayaneLibrary.Comments.Entities.Comment do
+  @moduledoc """
+  Determines comments schema
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias GayaneLibrary.Accounts.Entities.User
+  alias GayaneLibrary.Books.Entities.Book
+
+  @required [
+    :text_content,
+    :user_id,
+    :book_id
+  ]
+
+  schema "comments" do
+    field :text_content, :string
+
+    belongs_to :user, User
+    belongs_to :book, Book
+
+    timestamps()
+  end
+
+  def create_changeset(%__MODULE__{} = comment, attrs) do
+    comment
+    |> cast(attrs, @required)
+    |> validate_required(@required)
+    |> assoc_constraint(:user)
+    |> assoc_constraint(:book)
+  end
+end
