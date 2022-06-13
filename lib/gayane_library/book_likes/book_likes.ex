ca79a206910a726de526e1a1bb122f616b7f5860
Gayane <g_arshakyan@list.ru>
2022-06-13 17:51:18 +0300

    create book_like context

---
+++ lib/gayane_library/book_likes/book_likes.ex
@@ -0,0 +1,23 @@
+defmodule GayaneLibrary.BookLikes do
+  @moduledoc """
+  Describes the context of the book
+  """
+
+  alias GayaneLibrary.BookLikes.Commands.{
+    CreateBookLike,
+    DeleteBookLike
+  }
+
+  alias GayaneLibrary.BookLikes.Queries.{
+    GetBookLike,
+    ListBookLikes
+  }
+
+  # Commands
+  defdelegate create_book_like(attrs), to: CreateBookLike, as: :process
+  defdelegate delete_book_like(book_like), to: DeleteBookLike, as: :process
+
+  # Queries
+  defdelegate get_book_like(id), to: GetBookLike, as: :process
+  defdelegate list_book_likes(book), to: ListBookLikes, as: :process
+end
