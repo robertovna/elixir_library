9cf6b8f03eedac9c7e576d0a2068e10a045e2ca2
Gayane <g_arshakyan@list.ru>
2022-06-13 14:54:13 +0300

    create comment like context

---
+++ lib/gayane_library/comment_likes/comment_likes.ex
@@ -0,0 +1,23 @@
+defmodule GayaneLibrary.CommentLikes do
+  @moduledoc """
+  Describes the context of the book
+  """
+
+  alias GayaneLibrary.CommentLikes.Commands.{
+    CreateCommentLike,
+    DeleteCommentLike
+  }
+
+  alias GayaneLibrary.CommentLikes.Queries.{
+    GetCommentLike,
+    ListCommentLikes
+  }
+
+  # Commands
+  defdelegate create_comment_like(attrs), to: CreateCommentLike, as: :process
+  defdelegate delete_comment_like(comment_like), to: DeleteCommentLike, as: :process
+
+  # Queries
+  defdelegate get_comment_like(id), to: GetCommentLike, as: :process
+  defdelegate list_comment_likes(comment), to: ListCommentLikes, as: :process
+end
