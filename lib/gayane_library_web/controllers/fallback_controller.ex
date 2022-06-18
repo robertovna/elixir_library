c5bc5cd3c4e4cec3380ccd2cc36e1f63f08008be
Gayane <g_arshakyan@list.ru>
2022-06-18 19:28:20 +0300

    added 422 error view (unprocessable)

--- lib/gayane_library_web/controllers/fallback_controller.ex
+++ lib/gayane_library_web/controllers/fallback_controller.ex
@@ -1,6 +1,8 @@
 defmodule GayaneLibraryWeb.FallbackController do
   use GayaneLibraryWeb, :controller
 
+  alias Ecto.Changeset
+
   def call(%Conn{} = conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
@@ -13,5 +15,12 @@ defmodule GayaneLibraryWeb.FallbackController do
     |> put_status(:forbidden)
     |> put_view(ErrorView)
     |> render("403.json", message: "You are not authorized to perform this action.")
+  end
+
+  def call(%Conn{} = conn, {:error, %Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(ErrorView)
+    |> render("422.json", changeset: changeset)
   end
 end
--- lib/gayane_library_web/views/error_view.ex
+++ lib/gayane_library_web/views/error_view.ex
@@ -1,6 +1,7 @@
 defmodule GayaneLibraryWeb.ErrorView do
   use GayaneLibraryWeb, :view
 
+  alias Ecto.Changeset
   alias GayaneLibrary.Changeset.Error
 
   def render("404.json", %{}) do
@@ -13,5 +14,15 @@ defmodule GayaneLibraryWeb.ErrorView do
 
   def render("401.json", %{error: %Error{} = error}) do
     %{errors: [Map.from_struct(error)]}
+  end
+
+  def render("422.json", %{changeset: %Changeset{errors: errors}}) do
+    %{errors: convert_errors(errors)}
+  end
+
+  defp convert_errors(errors) do
+    Enum.map(errors, fn {field, {details, _details}} ->
+      %{field: field, code: details}
+    end)
   end
 end
