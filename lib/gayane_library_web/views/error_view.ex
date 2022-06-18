a8126c17a6d2d40f906f77fd405dbda0607d7a53
Gayane <g_arshakyan@list.ru>
2022-06-18 18:58:52 +0300

    added view for 401(auth error)

--- lib/gayane_library_web/views/error_view.ex
+++ lib/gayane_library_web/views/error_view.ex
@@ -1,11 +1,17 @@
 defmodule GayaneLibraryWeb.ErrorView do
   use GayaneLibraryWeb, :view
 
+  alias GayaneLibrary.Changeset.Error
+
   def render("404.json", %{}) do
     %{errors: ["Not found"]}
   end
 
   def render("403.json", %{message: message}) do
     %{errors: [message]}
+  end
+
+  def render("401.json", %{error: %Error{} = error}) do
+    %{errors: [Map.from_struct(error)]}
   end
 end
