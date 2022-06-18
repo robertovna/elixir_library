2d31aaff918b5d0b66f0ed6ea96c5f0f349a5ed5
Gayane <g_arshakyan@list.ru>
2022-06-19 01:16:58 +0300

    extend book update test

--- test/gayane_library/books/commands/update_book_test.exs
+++ test/gayane_library/books/commands/update_book_test.exs
@@ -8,5 +8,11 @@ defmodule GayaneLibrary.Books.Commands.UpdateBookTest do
     attrs = %{name: "updated_name"}
     assert {:ok, updated_book} = Books.update_book(book, attrs)
     assert updated_book.name == attrs.name
+    assert updated_book.id == book.id
+    assert updated_book.text_content == book.text_content
+    assert updated_book.year == book.year
+    assert updated_book.user_id == book.user_id
+    assert updated_book.author == book.author
+    assert updated_book.edition == book.edition
   end
 end
